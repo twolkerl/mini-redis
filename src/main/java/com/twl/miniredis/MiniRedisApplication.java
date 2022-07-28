89a6cf31fb4ac27bea4f636f76780b2713d65beb
Tiago Wolker <tiago.wolker.leite@gmail.com>
2022-07-28 10:05:01 -0300

    Primeiro commit

---
+++ src/main/java/com/twl/miniredis/MiniRedisApplication.java
@@ -0,0 +1,13 @@
+package com.twl.miniredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twl/miniredis/MiniRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twl.miniredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
